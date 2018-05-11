c512a603611e0e383ec6d4488aade3dd34e37482
Nils Otto Johnansen <nils.otto.johansen@husbanken.no>
2018-05-11 16:53:22 +0200

    tedt

--- src/main/java/no/hus/demo/DemoApplication.java
+++ src/main/java/no/hus/demo/DemoApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+
 	public static void main(String[] args) {
+		System.out.println("notto was here");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
